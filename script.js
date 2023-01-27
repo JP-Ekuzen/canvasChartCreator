5879d2f539652b4d7da9eb778ba68a833d9431b7
JP-Ekuzen <pwdjpokusa@gmail.com>
2023-01-27 21:36:32 +0100

    Added showing function pattern.

--- script.js
+++ script.js
@@ -1,19 +1,42 @@
 var chartType
 
-function changePatternImage () {
-    const obj = document.getElementById("function-pattern-img")
+function changeFunctionPreview () {
+
+    function changeVisibility (visibile) {
+        const obj = document.getElementById("chart-function-pattern")
+        if (visibile) obj.style.display = 'block';
+        else obj.style.display = 'none';
+    }
+
+    function editImage (url) {
+        const obj = document.getElementById("function-pattern-img")
+        obj.setAttribute('src', url);
+
+        if (url) changeVisibility(true);
+        else changeVisibility(false);
+    }
 
     switch (chartType) {
-        case '': obj.setAttribute('src', ""); break;
-        case 'prosta': obj.setAttribute('src', "https://latex.codecogs.com/svg.latex?\\Large&space;y=ax+b"); break;
-        case 'parabola': obj.setAttribute('src', "https://latex.codecogs.com/svg.latex?\\Large&space;f(x)={x}^2"); break;
-        case 'hiperbola': obj.setAttribute('src', "https://latex.codecogs.com/svg.latex?\\Large&space;f(x)=\\frac{a}{x}"); break;
-        case 'logarytmiczna': obj.setAttribute('src', "https://latex.codecogs.com/svg.latex?\\Large&space;f(x)=log_{a}x"); break;
+        case '': {
+            editImage('');
+        }; break;
+        case 'linear': {
+            editImage('https://latex.codecogs.com/svg.latex?\\Large&space;y=ax+b');
+        }; break;
+        case 'quadratic': {
+            editImage('https://latex.codecogs.com/svg.latex?\\Large&space;f(x)={x}^2');
+        }; break;
+        case 'hyperbola': {
+            editImage('https://latex.codecogs.com/svg.latex?\\Large&space;f(x)=\\frac{a}{x}');
+        }; break;
+        case 'logarithmic': {
+            editImage('https://latex.codecogs.com/svg.latex?\\Large&space;f(x)=log_{a}x');
+        }; break;
     }
 }
 
 function changeSettings() {
     chartType = document.getElementById("chart-type-select").value;
 
-    changePatternImage()
+    changeFunctionPreview()
 }
