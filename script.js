558af38579c12b0feb4da26df08bee9c7130b548
JP-Ekuzen <pwdjpokusa@gmail.com>
2023-02-13 18:18:08 +0100

    Added creating fields.

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 let chartType = null;
 
-function changeChartType (type) {
+function changeChartType (type, parameters) {
     chartType = type
-    alert(chartType);
+    console.log(chartType);
+
+    showFunctionParameters(parameters);
+}
+
+function showFunctionParameters(parameters) {
+    document.getElementById('function-parameters').style.display = 'block';
+    document.getElementById('function-parameters-inputs').innerHTML = '';
+
+    inputsDiv = document.getElementById('function-parameters-inputs');
+
+    for (symbol of parameters) {
+        let inputField = document.createElement('div');
+        inputField.innerHTML = `<label for="chart-input-${symbol}">Argument (${symbol})</label>
+                                <input type="text" class="input-parameter" id="chart-input-${symbol}" name="chart-input-${symbol}" required>`
+
+        inputsDiv.appendChild(inputField);
+    }
+
+    let submitButton = document.createElement('div');
+    submitButton.innerHTML = `<button class="btn btn-blue">Wygeneruj wykres</button>`
+    inputsDiv.appendChild(submitButton);
+
 }
