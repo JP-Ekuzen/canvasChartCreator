f6fe7b638783ec55bfdbdd071f09cabb5c0332ba
JP-Ekuzen <pwdjpokusa@gmail.com>
2023-02-13 23:49:35 +0100

    Completed generating image with function pattern.

--- script.js
+++ script.js
@@ -8,21 +8,39 @@ function changeChartType (type, parameters) {
 }
 
 function showFunctionParameters(parameters) {
+    // Pokazywanie i czyszczenie wszystkiego w divie.
     document.getElementById('function-parameters').style.display = 'block';
     document.getElementById('function-parameters-inputs').innerHTML = '';
 
     inputsDiv = document.getElementById('function-parameters-inputs');
 
+    // Generowanie url z obrazkiem funkcji.
+    let mathUrlFragment
+    const mainUrl = 'https://latex.codecogs.com/svg.latex?\\Large&space;'
+
+    switch (chartType) {
+        case 'linear': mathUrlFragment = 'y=ax+b'; break;
+        case 'quadratic': mathUrlFragment = 'f(x)={ax}^2+bx+c'; break;
+        case 'hyperbole': mathUrlFragment = 'f(x)=\\frac{a}{x}'; break;
+        case 'logarithmic': mathUrlFragment = 'f(x)=log_{a}x'; break;
+        case 'exponential': mathUrlFragment = 'f(x)={a}^x'; break;
+    }
+
+    let imgFunctionPattern = document.createElement('div');
+    imgFunctionPattern.innerHTML = `<img src="${mainUrl + mathUrlFragment}" style="margin:20px;"></img>`
+    inputsDiv.appendChild(imgFunctionPattern);
+
+    // Dodawanie pól z wartościami do uzupełnienia.
     for (symbol of parameters) {
         let inputField = document.createElement('div');
-        inputField.innerHTML = `<label for="chart-input-${symbol}">Argument (${symbol})</label>
-                                <input type="number" class="input-parameter" id="chart-input-${symbol}" name="chart-input-${symbol}" required>`
+        inputField.innerHTML = `<label for="chart-input-${symbol}">Parametr (${symbol})</label>
+                                <input type="number" class="input-parameter" id="parameter-input-${symbol}" name="parameter-input-${symbol}" required>`
 
         inputsDiv.appendChild(inputField);
     }
 
+    // Dodawanie przycisku potwierdzającego generowanie wykresu.
     let submitButton = document.createElement('div');
     submitButton.innerHTML = `<button class="btn btn-green">Wygeneruj wykres</button>`
     inputsDiv.appendChild(submitButton);
-
 }
